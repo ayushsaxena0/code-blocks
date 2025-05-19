0e5584eeb998175f7b01cfbeb00287098f796245
Ayush <ayushsaxena917@gmail.com>
2025-05-19 17:31:02 +0530

    Adding new blocks

--- code.js
+++ code.js
@@ -463,3 +463,52 @@ module.exports = router;
 </body>
 </html>
 */
+
+// Server.js template
+const express = require("express");
+const app = express();
+const mongoose = require("mongoose");
+const session = require("express-session");
+const MongoStore = require("connect-mongo");
+const passport = require("passport");
+const flash = require("express-flash");
+const logger = require("morgan");
+const homeRoutes = require("./routes/home");
+const todoRoutes = require("./routes/todos");
+const connectDB = require("./config/db");
+
+require("dotenv").config({ path: "./config/.env" });
+
+// Passport config
+require("./config/passport")(passport);
+
+connectDB();
+
+app.set("view engine", "ejs");
+app.use(express.static("public"));
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+app.use(logger("dev"));
+
+// Sessions
+app.use(
+  session({
+    secret: "keyboard cat",
+    resave: false,
+    saveUninitialized: false,
+    store: MongoStore.create({ mongoUrl: process.env.DB_STRING }),
+  })
+);
+
+// Passport middleware
+app.use(passport.initialize());
+app.use(passport.session());
+
+app.use(flash());
+
+app.use("/", homeRoutes);
+app.use("/todos", todoRoutes);
+
+app.listen(process.env.PORT || 3000, () => {
+  console.log("Server is listening");
+});
